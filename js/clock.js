48ddd025302f6fc11d9deca1ec5bca62d04a1735
Darren Fitzpatrick <dfitzpatrick12@gmail.com>
2017-12-14 20:16:14 +0000

    added a clock feature

---
+++ js/clock.js
@@ -0,0 +1,29 @@
+var locale = 'en-EN';
+var clock = {
+	hours: document.querySelector('.m-clock .hours').firstChild,
+	minutes: document.querySelector('.m-clock .minutes').firstChild,
+	date: document.querySelector('.m-date').firstChild
+};
+render();
+
+function padZero(number) {
+	var str = number.toString();
+	return str.length < 2 ? '0' + str : str;
+}
+
+function render() {
+	var now = new Date();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+
+	clock.hours.data = hours;
+	clock.minutes.data = padZero(minutes);
+	clock.date.data = now.toLocaleString(locale, {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric'
+	});
+
+	setTimeout(render, (60 - seconds) * 1000);
+}
